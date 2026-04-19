2a505c8b87489094ca8cd8c5ecbcbee9c0e24668
Joey Shi <joeyshi12@gmail.com>
2026-04-18 23:44:36 -0700

    Added keymap for lazy sync

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,6 @@ vim.g.maplocalleader = " "
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<C-n>", vim.cmd.tabn)
 vim.keymap.set("n", "<C-p>", vim.cmd.tabp)
+
+-- Lazy
+vim.keymap.set("n", "<leader>ls", "<cmd>Lazy sync<cr>", { desc = "Lazy sync" })
